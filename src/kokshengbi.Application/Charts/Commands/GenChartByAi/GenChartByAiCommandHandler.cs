18b71c7ee3da701e203332c225bb828573eb62c7
koksheng <koksheng0406@gmail.com>
2024-07-26 00:19:04 +0800

    update GenChartByAi

--- src/kokshengbi.Application/Charts/Commands/GenChartByAi/GenChartByAiCommandHandler.cs
+++ src/kokshengbi.Application/Charts/Commands/GenChartByAi/GenChartByAiCommandHandler.cs
@@ -34,13 +34,14 @@ namespace kokshengbi.Application.Charts.Commands.GenChartByAi
             //// 1. Verify User using userId in userState
             //var safetyUser = await _currentUserService.GetCurrentUserAsync(userState);
 
-            // 压缩后的数据
+            // 对内容进行压缩
             var csvData = await _excelService.ConvertExcelToCsvAsync(command.file);
 
             // 用户输入
             StringBuilder userInput = new StringBuilder();
             userInput.Append("你是一个数据分析师，接下来我会给你我的分析目标和原始数据，请告诉我分析结论。").Append("\n");
             userInput.Append("分析目标：").Append(goal).Append("\n");
+            // 压缩后的数据
             userInput.Append("数据：").Append(csvData).Append("\n");
             return ResultUtils.success(userInput.ToString());
 
